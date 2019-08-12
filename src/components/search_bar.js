18c74af2865db1c2ff4a03350177cd93f87419b6
Gemma Massó <gmasso@visual-engin.com>
2019-08-12 13:52:04 +0200

    Created component SearchBar

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className={"search-bar"}>
+        <form className={"input-group"}>
+          <input
+            value={this.props.newValue}
+            onChange={this.props.onChange}
+            placeholder="Search"
+          />
+          <img
+            className={"search-icon"}
+            src="https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/ic_search.png"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
